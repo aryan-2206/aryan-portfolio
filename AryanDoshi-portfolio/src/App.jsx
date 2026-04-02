40d371dc339888130691f2989bdc5130022fae78
aryan-2206 <aryandoshi22@gmail.com>
2026-04-03 02:13:51 +0530

    background ui

---
+++ AryanDoshi-portfolio/src/App.jsx
@@ -0,0 +1,29 @@
+import Navbar from "./components/Navbar"
+import Home from "./sections/Home"
+import About from "./sections/About"
+import Skills from "./sections/Skills"
+import Education from "./sections/Education"
+import Testimonials from "./sections/Testimonials"
+import Footer from "./sections/Footer"
+import Achievements from "./sections/Achievements"
+import Contact from "./sections/Contact"
+import Projects from "./sections/Projects"
+import ParticlesBackground from "./components/ParticlesBackground"
+
+export default function App() {
+  return(
+    <div className="relative gradient text-white">
+      <ParticlesBackground></ParticlesBackground>
+      <Navbar/>
+      <Home></Home>
+      <About></About>
+      <Skills></Skills>
+      <Projects></Projects>
+      <Education></Education>
+      <Achievements></Achievements>
+      <Testimonials></Testimonials>
+      <Contact></Contact>
+      <Footer></Footer>
+    </div>
+  )
+}
---
+++ AryanDoshi-portfolio/src/sections/Education.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Education = () => {
+  return (
+    <div className='w-full h-screen'>
+      Education
+    </div>
+  )
+}
+
+export default Education
